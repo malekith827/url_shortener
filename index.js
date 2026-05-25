7119dd5cd5e2217b876fa86ad322616dd3697262
Malekith827 <natrajanabhishek2021@gmail.com>
2026-05-25 15:38:30 +0530

    updated port

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.get('/:code' , async(req, res) => {
     res.redirect(result.rows[0].long_url);
 });
 
- app.listen(3000, () => {
-     console.log('Server is running on port 3000');
- });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(
+        `Server running on port ${PORT}`
+    );
+});
